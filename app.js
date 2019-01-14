d8a2fa0f3a00dd546b2a45e67c3fc0c8c7900ef8
Leif Lampater <leiflampater@gmail.com>
2019-01-14 21:06:29 +0100

    ADD token is used to redirect routes based on auth status

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 app.use(isAuth);
 
 app.use('/graphql', graphqlHttp({
@@ -20,7 +29,7 @@ app.use('/graphql', graphqlHttp({
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-y4p3c.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, {useNewUrlParser: true})
     .then(() => {
-        app.listen(3000);
+        app.listen(8000);
     }).catch(err => {
     console.log(err);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,43 @@ import AuthPage from "./pages/Auth";
 import Bookings from "./pages/Bookings";
 import Events from "./pages/Events";
 import MainNavigation from "./components/navigation/MainNavigation";
+import AuthContext from './context/auth-context';
 
 class App extends Component {
+    state = {
+        token: null,
+        userId: null,
+    };
+
+    login = (token, userId, tokenExpiration) => {
+        this.setState({token: token, userId: userId});
+    };
+    logout = () => {
+        this.setState({token: null, userId: null});
+    };
+
     render() {
         return (
             <BrowserRouter>
                 <React.Fragment>
-                    <MainNavigation/>
-                    <main className="main-content">
-                        <Switch>
-                            <Redirect from="/" to="/auth" exact/>
-                            <Route path="/auth" component={AuthPage}/>
-                            <Route path="/events" component={Events}/>
-                            <Route path="/bookings" component={Bookings}/>
-                        </Switch>
-                    </main>
+                    <AuthContext.Provider value={{
+                        token: this.state.token,
+                        userId: this.state.userId,
+                        login: this.login,
+                        logout: this.logout
+                    }}>
+                        <MainNavigation/>
+                        <main className="main-content">
+                            <Switch>
+                                {!this.state.token && <Redirect from="/" to="/auth" exact/>}
+                                {this.state.token && <Redirect from="/" to="/events" exact/>}
+                                {this.state.token && <Redirect from="/auth" to="/events" exact/>}
+                                {!this.state.token &&   <Route path="/auth" component={AuthPage}/>}
+                                <Route path="/events" component={Events}/>
+                                {this.state.token &&   <Route path="/bookings" component={Bookings}/>}
+                            </Switch>
+                        </main>
+                    </AuthContext.Provider>
                 </React.Fragment>
             </BrowserRouter>
         );
